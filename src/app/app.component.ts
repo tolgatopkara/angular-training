4690fd808a7639e4f652637a68d4643a5b67bf25
Tolga <tolga.topkara96@gmail.com>
2023-01-07 06:49:22 +0300

    folder-fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,20 @@
 import { Component } from '@angular/core';
-import { PropertyBindingComponent } from './property-binding/property-binding.component';
 import { HeaderComponent } from "./header/header.component";
-import { EventBindingComponent } from "./event-binding/event-binding.component";
-import { KeyboardEventsComponent } from "./keyboard-events/keyboard-events.component";
+import { EventBindingComponent } from "./angular/event-binding/event-binding.component";
+import { KeyboardEventsComponent } from "./angular/keyboard-events/keyboard-events.component";
+import { PropertyBindingComponent } from "./angular/property-binding/property-binding.component";
 
 @Component({
     standalone: true,
     selector: 'app-root',
     template: `
     <app-header></app-header>
-     <!-- <app-property-binding></app-property-binding> -->
-     <!-- <app-event-binding></app-event-binding> -->
+    <!-- <app-property-binding></app-property-binding>
+      <app-event-binding></app-event-binding> -->
      <app-keyboard-events></app-keyboard-events>
 
      `,
-    imports: [PropertyBindingComponent, HeaderComponent, EventBindingComponent, KeyboardEventsComponent]
+    imports: [ HeaderComponent,  EventBindingComponent, KeyboardEventsComponent, PropertyBindingComponent]
 })
 export class AppComponent {
   title = 'angular-training';
